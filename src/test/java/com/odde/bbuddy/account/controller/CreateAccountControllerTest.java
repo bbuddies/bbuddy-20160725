627bf1e4b96cc69cd96df32080d28dc629fde36f
xiaomei <testcomt/users.noreply.github.com>
2016-07-27 14:57:37 +0800

    add return view name ut

--- src/test/java/com/odde/bbuddy/account/controller/CreateAccountControllerTest.java
+++ src/test/java/com/odde/bbuddy/account/controller/CreateAccountControllerTest.java
@@ -4,6 +4,7 @@ import com.odde.bbuddy.account.AccountService;
 import com.odde.bbuddy.account.domain.Account;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -12,14 +13,19 @@ import static org.mockito.Mockito.verify;
  */
 public class CreateAccountControllerTest {
 
+    AccountService service = mock(AccountService.class);
+    CreateAccountController controller = new CreateAccountController(service);
+    Account account = new Account("default_account",1000);
     @Test
     public void save_account_data_when_create_a_new_account(){
-        AccountService service = mock(AccountService.class);
-        CreateAccountController controller = new CreateAccountController(service);
-        Account account = new Account("default_account",1000);
-
         controller.createAccount(account);
 
         verify(service).saveAccount(account);
     }
+
+    @Test
+    public void back_to_create_account_page_when_create_a_new_account(){
+        assertEquals("create_account",controller.createAccount(account));
+    }
+
 }
